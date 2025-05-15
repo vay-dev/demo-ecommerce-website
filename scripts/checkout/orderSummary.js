6921cf8992d6bd7d2a756f24c0e5cf3caf9eab78
Viccodes <ayeniv69@gmail.com>
2025-05-15 14:13:40 +0100

    generated the order section and made the display element above it interactive

---
+++ scripts/checkout/orderSummary.js
@@ -0,0 +1,97 @@
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
+import { cart, calculateCartQuantity } from '../../data/cart.js';
+import { getProduct, products } from '../../data/products.js';
+import { formatCurrency } from '../utils/money.js';
+
+const today = dayjs();
+console.log(today.format('dddd, MM, DD'));
+
+export function renderOrderSummary() {
+  let cartSummaryHTML = '';
+
+cart.map((cartItem) => {
+
+    const productId = cartItem.productId;
+    const matchingProduct = getProduct(productId);
+    
+
+
+  cartSummaryHTML += `
+            <div class="cart-item-container">
+            <div class="delivery-date">
+              Delivery date: Wednesday, June 15
+            </div>
+
+            <div class="cart-item-details-grid">
+              <img class="product-image"
+                src="${matchingProduct.image}">
+
+              <div class="cart-item-details">
+                <div class="product-name">
+                  ${matchingProduct.name}
+                </div>
+                <div class="product-price">
+                  $${formatCurrency(matchingProduct.priceCents)}
+                </div>
+                <div class="product-quantity">
+                  <span>
+                    Quantity: <span class="quantity-label">(${cartItem.quantity})</span>
+                  </span>
+                  <span class="update-quantity-link link-primary">
+                    Edit
+                  </span>
+                  
+                </div>
+              </div>
+
+              <div class="delivery-options">
+                <div class="delivery-options-title">
+                  Choose a delivery option:
+                </div>
+
+                <div class="delivery-option">
+                  <input type="radio" class="delivery-option-input"
+                    name="delivery-option-${cartItem.productId}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Tuesday, June 21
+                    </div>
+                    <div class="delivery-option-price">
+                      FREE Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio" checked class="delivery-option-input"
+                    name="delivery-option-${cartItem.productId}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Wednesday, June 15
+                    </div>
+                    <div class="delivery-option-price">
+                      $4.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio" class="delivery-option-input"
+                    name="delivery-option-${cartItem.productId}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Monday, June 13
+                    </div>
+                    <div class="delivery-option-price">
+                      $9.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+  `
+
+  document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+  calculateCartQuantity();
+});
+
+}
